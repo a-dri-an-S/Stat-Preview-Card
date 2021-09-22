f8d9f8e726184c6b2f176749394f8d1a02899251
Adrian Salinas <adrian.salinas.1986@gmail.com>
2021-09-21 17:16:19 -0700

    added styles + breakpoint for mobile view

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
   return (
     <main className="app">
       <MainCard />
-      <div class="attribution">
+      {/* <div class="attribution">
         <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.</p> 
-        {/* <p>Coded by <a href="#">Your Name Here</a>.</p> */}
-      </div>
+        <p>Coded by <a href="#">Your Name Here</a>.</p>
+      </div> */}
     </main>
   );
 }
